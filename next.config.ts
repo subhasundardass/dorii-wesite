657d8bcc961b447542831dcfb01f2f196f9782ba
Subha Sundar Das <infolinematrix@gmail.com>
2026-01-16 18:21:14 +0530

    Updates

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
