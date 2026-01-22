15f90cf3760e2d1b937b49df19f50d99496d25fd
Subha Sundar Das <infolinematrix@gmail.com>
2026-01-22 11:12:02 +0530

    Sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://dorii.in', // your website URL
+  generateRobotsTxt: true,     // optional: generate robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+};
